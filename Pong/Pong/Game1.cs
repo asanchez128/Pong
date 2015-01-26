5c6c0c58b9cfe55cbbaf81b549a8606abd4fa7c5
Ama Sanchez <asanchez@harding.edu>
2015-01-26 11:49:50 -0600

    Deleted files in PongContent

--- Pong/Pong/Game1.cs
+++ Pong/Pong/Game1.cs
@@ -1,3 +1,13 @@
+/*
+ * Basketball Pong
+ * by Frank McCown, Harding University
+ * Spring 2012
+ * 
+ * Sounds: Creative Commons Sampling Plus 1.0 License.
+ * http://www.freesound.org/samplesViewSingle.php?id=34201
+ * http://www.freesound.org/samplesViewSingle.php?id=12658
+ */
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,161 +21,175 @@ using Microsoft.Xna.Framework.Media;
 
 namespace Pong
 {
-   /// <summary>
-   /// This is the main type for your game
-   /// </summary>
-   public class Game1 : Microsoft.Xna.Framework.Game
-   {
-      GraphicsDeviceManager graphics;
-      SpriteBatch spriteBatch;
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        private GraphicsDeviceManager graphics;
 
-      Texture2D ballSprite;
+        private Ball ball;
+        private Paddle paddle;
 
-      Vector2 ballPosition = Vector2.Zero;
+        private SoundEffect swishSound;
+        private SoundEffect crashSound;
 
-      Vector2 ballSpeed = new Vector2(150, 150);
+        // Used to delay between rounds 
+        private float delayTimer = 0;
 
-      Texture2D paddleSprite;
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
 
-      Vector2 paddlePosition;
+            ball = new Ball(this);
+            paddle = new Paddle(this);
 
-      SoundEffect swishSoundEffect;
+            Components.Add(ball);
+            Components.Add(paddle);             
 
-      SoundEffect crashSoundEffect;
-      public Game1()
-      {
-         graphics = new GraphicsDeviceManager(this);
-         Content.RootDirectory = "Content";
-      }
+            // Call Window_ClientSizeChanged when screen size is changed
+            this.Window.ClientSizeChanged += new EventHandler<EventArgs>(Window_ClientSizeChanged);
+        }
 
-      /// <summary>
-      /// Allows the game to perform any initialization it needs to before starting to run.
-      /// This is where it can query for any required services and load any non-graphic
-      /// related content.  Calling base.Initialize will enumerate through any components
-      /// and initialize them as well.
-      /// </summary>
-      protected override void Initialize()
-      {
-         // TODO: Add your initialization logic here
-         IsMouseVisible = true;
-         base.Initialize();
+        void Window_ClientSizeChanged(object sender, EventArgs e)
+        {
+            // Move paddle back onto screen if it's off
+            paddle.Y = GraphicsDevice.Viewport.Height - paddle.Height;
+            if (paddle.X + paddle.Width > GraphicsDevice.Viewport.Width)
+                paddle.X = GraphicsDevice.Viewport.Width - paddle.Width;
+        }
 
-         paddlePosition = new Vector2(graphics.GraphicsDevice.Viewport.Width / 2 - paddleSprite.Width / 2,
-            graphics.GraphicsDevice.Viewport.Height - paddleSprite.Height);
-      }
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // Make mouse visible
+            IsMouseVisible = true;
 
-      /// <summary>
-      /// LoadContent will be called once per game and is the place to load
-      /// all of your content.
-      /// </summary>
-      protected override void LoadContent()
-      {
-         // Create a new SpriteBatch, which can be used to draw textures.
-         spriteBatch = new SpriteBatch(GraphicsDevice);
+            // Set the window's title bar
+            Window.Title = "Basketball Pong!";
 
-         // TODO: use this.Content to load your game content here
+            graphics.ApplyChanges();
 
-         ballSprite = Content.Load<Texture2D>("basketball");
+            // Don't allow ball to move just yet
+            ball.Enabled = false;  
 
-         paddleSprite = Content.Load<Texture2D>("hand");
+            base.Initialize();
+        }
 
-         swishSoundEffect = Content.Load<SoundEffect>("swish");
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            swishSound = Content.Load<SoundEffect>(@"Audio\swish");
+            crashSound = Content.Load<SoundEffect>(@"Audio\crash");
+        }
 
-         crashSoundEffect = Content.Load<SoundEffect>("crash");
-      }
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
 
-      /// <summary>
-      /// UnloadContent will be called once per game and is the place to unload
-      /// all content.
-      /// </summary>
-      protected override void UnloadContent()
-      {
-         // TODO: Unload any non ContentManager content here
-      }
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Escape))
+                this.Exit();
 
-      /// <summary>
-      /// Allows the game to run logic such as updating the world,
-      /// checking for collisions, gathering input, and playing audio.
-      /// </summary>
-      /// <param name="gameTime">Provides a snapshot of timing values.</param>
-      protected override void Update(GameTime gameTime)
-      {
-         // Allows the game to exit
-         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
-            this.Exit();
-
-         // TODO: Add your update logic here
-
-         ballPosition += ballSpeed*(float) gameTime.ElapsedGameTime.TotalSeconds;
-
-         int maxX = GraphicsDevice.Viewport.Width - ballSprite.Width;
-         int maxY = GraphicsDevice.Viewport.Height - ballSprite.Height;
-
-         KeyboardState keyState = Keyboard.GetState();
-         if (keyState.IsKeyDown(Keys.Right))
-            paddlePosition.X += 5;
-         else if (keyState.IsKeyDown(Keys.Left))
-            paddlePosition.X -= 5;
-
-         if (ballPosition.X > maxX || ballPosition.X < 0)
-         {
-            ballSpeed.X *= -1;
-         }
-
-         if (ballPosition.Y < 0)
-         {
-            ballSpeed.Y *= -1;
-         }
-         else if (ballPosition.Y > maxY)
-         {
-            crashSoundEffect.Play();
-
-            ballPosition.Y = 0;
-            ballSpeed.X = 150;
-            ballSpeed.Y = 150;
-         }
-
-         Rectangle ballRectangle = new Rectangle((int)ballPosition.X, (int)ballPosition.Y,
-            ballSprite.Width, ballSprite.Height);
-
-         Rectangle handRectangle = new Rectangle((int)paddlePosition.X, (int)paddlePosition.Y, 
-            paddleSprite.Width, paddleSprite.Height);
-
-         if (ballRectangle.Intersects(handRectangle) && ballSpeed.Y > 0)
-         {
-            swishSoundEffect.Play();
-
-            ballSpeed.Y += 50;
-            if (ballSpeed.X < 0)
+            // Press F to toggle full-screen mode
+            if (Keyboard.GetState().IsKeyDown(Keys.F))
             {
-               ballSpeed.X -= 50;
-            }
-            else
-            {
-               ballSpeed.X += 50;
+                graphics.IsFullScreen = !graphics.IsFullScreen;
+                graphics.ApplyChanges();
             }
 
-            ballSpeed.Y *= -1;
-         }
-         base.Update(gameTime);
-      }
+            // Wait until a second has passed before animating ball 
+            delayTimer += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (delayTimer > 1)            
+                ball.Enabled = true;
+            
+            int maxX = GraphicsDevice.Viewport.Width - ball.Width;
+            int maxY = GraphicsDevice.Viewport.Height - ball.Height;
 
-      /// <summary>
-      /// This is called when the game should draw itself.
-      /// </summary>
-      /// <param name="gameTime">Provides a snapshot of timing values.</param>
-      protected override void Draw(GameTime gameTime)
-      {
-         GraphicsDevice.Clear(Color.CornflowerBlue);
+            // Check for bounce. Make sure to place ball back inside the screen
+            // or it could remain outside the screen on the next iteration and cause
+            // a back-and-forth bouncing logic error.
+            if (ball.X > maxX)
+            {
+                ball.ChangeHorzDirection();
+                ball.X = maxX;
+            }
+            else if (ball.X < 0)
+            {
+                ball.ChangeHorzDirection();
+                ball.X = 0;
+            }
 
-         // TODO: Add your drawing code here
+            if (ball.Y < 0)
+            {
+                ball.ChangeVertDirection();
+                ball.Y = 0;
+            }
+            else if (ball.Y > maxY)
+            {
+                // Game over - reset ball
+                crashSound.Play();
+                ball.Reset();
 
-         spriteBatch.Begin();
-         spriteBatch.Draw(ballSprite, ballPosition, Color.White);
-         spriteBatch.Draw(paddleSprite, paddlePosition, Color.White);
-         spriteBatch.End();
+                // Reset timer and stop ball's Update() from executing
+                delayTimer = 0;
+                ball.Enabled = false;
+            }
 
-         base.Draw(gameTime);
-      }
-   }
+            // Collision?  Check rectangle intersection between ball and hand
+            if (ball.Boundary.Intersects(paddle.Boundary) && ball.SpeedY > 0)
+            {
+                swishSound.Play();
+
+                // If hitting the side of the paddle the ball is coming toward, 
+                // switch the ball's horz direction
+                float ballMiddle = (ball.X + ball.Width) / 2;
+                float paddleMiddle = (paddle.X + paddle.Width) / 2;
+                if ((ballMiddle < paddleMiddle && ball.SpeedX > 0) ||
+                    (ballMiddle > paddleMiddle && ball.SpeedX < 0))
+                {
+                    ball.ChangeHorzDirection();
+                }
+
+                // Go back up the screen and speed up
+                ball.ChangeVertDirection();
+                ball.SpeedUp();                
+            }
+            
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.White);
+            
+            base.Draw(gameTime);
+        }
+    }
 }
